2d6a66fc00973918e33ec36c87df2d39336bac71
sneh-shin <snehashinde800@gmail.com>
2022-12-08 22:48:01 +0530

    api-gateway

--- src/main/java/com/niit/jap/controller/TrackController.java
+++ src/main/java/com/niit/jap/controller/TrackController.java
@@ -15,7 +15,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v2")
 public class TrackController {
     private TrackServiceImpl service;
 
